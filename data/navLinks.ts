9f6ab035c7bfb8842307be7c979f16e6e44858d4
June Kim <gjune6723@gmail.com>
2024-11-25 23:29:22 +0900

    docs: Shorts intro 작성

--- data/navLinks.ts
+++ data/navLinks.ts
@@ -1,6 +1,7 @@
 const navLinks: Link[] = [
-  { title: "Home", link: "/" },
-  { title: "Blog", link: "/blog" },
-  { title: "About", link: "/about" },
+  { title: 'Home', link: '/' },
+  { title: 'Blog', link: '/blog' },
+  { title: 'Shorts', link: '/shorts' },
+  { title: 'About', link: '/about' },
 ];
 export default navLinks;
---
+++ pages/shorts.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Container from 'components/Container';
+import { allPosts, Post } from 'contentlayer/generated';
+import { getCustomMeta } from 'data/metaData';
+import { GetStaticProps, InferGetStaticPropsType } from 'next';
+import { useMDXComponent } from 'next-contentlayer/hooks';
+
+export default function Shorts({ shortsIntro }: InferGetStaticPropsType<typeof getStaticProps>) {
+  // shorts 페이지에 들어가는 컨텐츠들은 짧은 형식의 blog post 이다.
+  const MDXComponent = useMDXComponent(shortsIntro.body.code);
+  const customMeta = getCustomMeta({
+    title: shortsIntro.title,
+    description: shortsIntro.description,
+    date: shortsIntro.date,
+  });
+
+  return (
+    <Container customMeta={customMeta}>
+      <div className="mt-10 prose dark:prose-invert">
+        <h1 className="text-white">{shortsIntro.title}</h1>
+        <MDXComponent />
+      </div>
+    </Container>
+  );
+}
+
+export const getStaticProps: GetStaticProps<{ shortsIntro: Post }> = async () => {
+  const shortsIntro = allPosts.filter(post => post.title === 'Shorts 소개')[0];
+  if (!shortsIntro) return { notFound: true };
+  return {
+    props: { shortsIntro },
+  };
+};
