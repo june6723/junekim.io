4bc82e2eb82d4006aa3784252cc37169e339fd06
June Kim <gjune6723@gmail.com>
2024-11-25 23:11:01 +0900

    style: Draft는 상단에 표시 되도록 변경

--- components/DraftPreview.tsx
+++ components/DraftPreview.tsx
@@ -6,11 +6,16 @@ type DraftPreviewProps = {
 };
 
 const DraftPreview = (props: DraftPreviewProps) => {
-  const { date, title, description, _raw } = props.draft;
+  const { date, title, description, _raw, type } = props.draft;
   return (
     <Link href={`/blog/${_raw.flattenedPath}`} passHref>
       <a className={`w-full my-7 hover:-translate-x-1.5`}>
-        <div className={`font-medium text-xs text-gray-400`}>{format(parseISO(date), 'LLLL d, yyyy')}</div>
+        <div className={`flex items-center gap-2`}>
+          <div className={`font-medium text-xs text-gray-400`}>{format(parseISO(date), 'LLLL d, yyyy')}</div>
+          <span className={`inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold `}>
+            {type}
+          </span>
+        </div>
         <div className={`font-extrabold text-2xl mt-2`}>{title}</div>
         <div className={`font-medium text-gray-600 text-xl mt-1`}>{description}</div>
       </a>
