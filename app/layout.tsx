4cfba244117fed2a526668e376f86c29273b0844
June Kim <gjune6723@gmail.com>
2024-12-08 22:22:37 +0900

    fix: tailwind config에 app router도 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
-import '@/styles/globals.css';
-import '@/styles/prism-vsc-dark-plus.css';
+import '../styles/globals.css';
+import '../styles/prism-vsc-dark-plus.css';
 
 export default function RootLayout({
   // Layouts must accept a children prop.
